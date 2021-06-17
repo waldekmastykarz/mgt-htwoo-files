7dfeb41b6c61bdac5782d52f50274100c2a46774
waldekmastykarz <waldek@mastykarz.nl>
2021-06-17 16:02:39 +0200

    Refactored replacing content placeholders

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function loadFileThumbnail(file) {
+function loadFileThumbnail(file, templateElement) {
   if (file.folder) { return; }
 
   mgt.Providers.globalProvider.graph
@@ -6,38 +6,42 @@ function loadFileThumbnail(file) {
     .get()
     .then(thumbnail => {
       const url = URL.createObjectURL(thumbnail);
-      const placeholder = document.querySelector(`.hoo-cardimage.i${file.id}`);
-      placeholder.innerHTML = `<img src="${url}" width="320" height="180" alt="">`;
+      templateElement
+        .querySelector(`.hoo-cardimage.i${file.id}`)
+        .innerHTML = `<img src="${url}" width="320" height="180" alt="">`;
     }, err => { });
 }
 
-function loadUserAvatar(file) {
+function loadUserAvatar(file, templateElement) {
   mgt.Providers.globalProvider.graph
     .api(`/users/${file.lastModifiedBy.user.id}/photo/$value`)
     .get()
     .then(photo => {
       const url = URL.createObjectURL(photo);
-      const placeholder = document.querySelector(`.hoo-avatar.i${file.id}`);
-      placeholder.innerHTML = `<img src="${url}" alt="" class="hoo-avatar-img" loading="lazy">`;
+      templateElement
+        .querySelector(`.hoo-avatar.i${file.id}`)
+        .innerHTML = `<img src="${url}" alt="" class="hoo-avatar-img" loading="lazy">`;
     }, err => { });
 }
 
-function loadSiteTitle(file) {
+function loadSiteTitle(file, templateElement) {
   mgt.Providers.globalProvider.graph
     .api(`/drives/${file.parentReference.driveId}`)
     .select('name')
     .get()
     .then(res => {
-      document.querySelector(`.hoo-cardlocation.i${file.id}`).innerHTML = res.name;
+      templateElement
+        .querySelector(`.hoo-cardlocation.i${file.id}`)
+        .innerHTML = res.name;
     }, err => { });
 }
 
 const fileList = document.querySelector('mgt-file-list');
 fileList.addEventListener('templateRendered', e => {
   e.detail.context.files.forEach(file => {
-    loadFileThumbnail(file);
-    loadUserAvatar(file);
-    loadSiteTitle(file);
+    loadFileThumbnail(file, e.detail.element);
+    loadUserAvatar(file, e.detail.element);
+    loadSiteTitle(file, e.detail.element);
   });
 });
 fileList.templateContext = {
